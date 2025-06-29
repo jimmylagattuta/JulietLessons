a7fc318c26d6824ac9de095d33b9c8a84a6fd81f
James Lagattuta <jimmy.lagattuta@gmail.com>
2025-06-29 14:44:44 -0700

    adding new lesson feature

--- client/src/components/NewLesson.jsx
+++ client/src/components/NewLesson.jsx
@@ -3,9 +3,12 @@ import './NewLesson.css';
 
 export default function NewLesson() {
   return (
-    <div className="new-lesson-page">
-      <h1 className="new-lesson-title">Create a New Lesson</h1>
-      {/* later: form fields, stepper, etc */}
+    <div className="lesson-page">
+        <aside className="lesson-sidebar">Sidebar</aside>
+        <div className="new-lesson-page">
+            <h1 className="new-lesson-title">Create a New Lesson</h1>
+            {/* later: form fields, stepper, etc */}
+        </div>
     </div>
   );
 }
