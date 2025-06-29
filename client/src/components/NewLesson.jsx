20fa414f8a3a17fb017c4995b6d08f62870add7c
James Lagattuta <jimmy.lagattuta@gmail.com>
2025-06-29 15:10:21 -0700

    adding new lesson feature

--- client/src/components/NewLesson.jsx
+++ client/src/components/NewLesson.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
-import './NewLesson.css';
+import React from 'react'
+import './NewLesson.css'
 
 export default function NewLesson() {
   return (
     <div className="lesson-page">
-        <aside className="lesson-sidebar">Sidebar</aside>
-        <div className="new-lesson-page">
-            <h1 className="new-lesson-title">Create a New Lesson</h1>
-            {/* later: form fields, stepper, etc */}
+      <aside className="lesson-sidebar">Sidebar</aside>
+      <div className="new-lesson-page">
+        <div className="new-lesson-header">
+          <button
+            className="new-lesson-add"
+            onClick={() => {
+              /* no-op for now */
+            }}
+          >
+            +
+          </button>
+          <h1 className="new-lesson-title">Create a New Lesson</h1>
         </div>
+        {/* later: form fields, stepper, etc */}
+      </div>
     </div>
-  );
+  )
 }
