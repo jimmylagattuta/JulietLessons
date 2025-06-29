0c9a6ba73aa2c8b7c975ed44c829e2bf3055c691
James Lagattuta <jimmy.lagattuta@gmail.com>
2025-06-29 15:20:47 -0700

    adding new lesson feature

--- client/src/components/NewLesson.jsx
+++ client/src/components/NewLesson.jsx
@@ -2,19 +2,22 @@ import React from 'react'
 import './NewLesson.css'
 
 export default function NewLesson() {
+  const handleClickHeader = () => {
+    // no-op for now
+  }
+
   return (
     <div className="lesson-page">
       <aside className="lesson-sidebar">Sidebar</aside>
       <div className="new-lesson-page">
-        <div className="new-lesson-header">
-          <button
-            className="new-lesson-add"
-            onClick={() => {
-              /* no-op for now */
-            }}
-          >
-            +
-          </button>
+        <div
+          className="new-lesson-header"
+          onClick={handleClickHeader}
+          role="button"
+          tabIndex={0}
+          onKeyPress={e => { if (e.key === 'Enter') handleClickHeader() }}
+        >
+          <button className="new-lesson-add" type="button">+</button>
           <h1 className="new-lesson-title">Create a New Lesson</h1>
         </div>
         {/* later: form fields, stepper, etc */}
