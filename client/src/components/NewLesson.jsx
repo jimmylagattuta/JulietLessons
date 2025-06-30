4f90ff17863daeaa26d91a23091ae43279d9500d
James Lagattuta <jimmy.lagattuta@gmail.com>
2025-06-30 00:01:25 -0700

    new lesson feature complete, turning off demoMode

--- client/src/components/NewLesson.jsx
+++ client/src/components/NewLesson.jsx
@@ -7,7 +7,7 @@ import './NewLesson.css'
  * Set DEMO = true while developing to auto-fill 
  * Title, Objective, and two At a Glance bullets.
  */
-const DEMO = true
+const DEMO = false
 
 const SECTION_LABELS = {
   warm_up:         'Warm Ups',
