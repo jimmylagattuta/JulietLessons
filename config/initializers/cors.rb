7aacb28c59bba5b21b4515fc695d5aa2f412737c
James Lagattuta <jimmy.lagattuta@gmail.com>
2025-07-02 23:45:36 -0700

    bugfixing cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,15 +1,17 @@
 # config/initializers/cors.rb
-
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins [
-      'http://localhost:3000',
-      'https://juliet-lessons-e75b65ff418d.herokuapp.com'
-    ]
+    # In production only allow your real front-end
+    if Rails.env.production?
+      origins ENV.fetch("CORS_ORIGINS", "https://juliet-lessons-e75b65ff418d.herokuapp.com")
+    else
+      # in dev you can add localhost, ngrok, review apps, etc.
+      origins "http://localhost:3000", "https://*.ngrok-free.app"
+    end
 
-    resource '/api/*',
+    resource "/api/*",
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      methods: %i[get post put patch delete options head],
       credentials: false
   end
 end
