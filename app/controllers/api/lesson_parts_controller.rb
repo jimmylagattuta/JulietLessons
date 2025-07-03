4954908494f42a9c6b5137cf2cf6ce6b8a715395
James Lagattuta <jimmy.lagattuta@gmail.com>
2025-07-02 22:20:47 -0700

    rewiring new lesson to new lesson part

--- app/controllers/api/lesson_parts_controller.rb
+++ app/controllers/api/lesson_parts_controller.rb
@@ -1,13 +1,33 @@
-class Api::LessonPartsController < ApplicationController
-  def create
-    puts "*" * 100
-    puts "new lesson part params"
-    puts params.inspect
-    puts "*" * 100
+module Api
+  class LessonPartsController < ApplicationController
+    # If you’re using cookies-based CSRF, you can disable it for API calls:
+    protect_from_forgery with: :null_session
 
-    render json: {
-      message: "Received lesson part creation request",
-      received: params.to_unsafe_h
-    }, status: :ok
+    def create
+      @lesson_part = LessonPart.new(lesson_part_params)
+
+      if @lesson_part.save
+        render json: @lesson_part, status: :created
+      else
+        render json: { errors: @lesson_part.errors.full_messages },
+               status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def lesson_part_params
+      params
+        .require(:lesson_part)
+        .permit(
+          :section_type,
+          :title,
+          :body,
+          :time,
+          :age_group,
+          :level,
+          files: []            # permit an array of attached PDFs
+        )
+    end
   end
 end
